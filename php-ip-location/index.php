5fa0c5614c33ae68fed29b04dc25501cd87ceb9b
DanielaOZ <65467658+DanielaOZ@users.noreply.github.com>
2022-10-26 16:30:33 -0500

    Update index.php

--- php-ip-location/index.php
+++ php-ip-location/index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
+$var= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
+echo $var;
