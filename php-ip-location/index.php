72c441728e9eadb3dee01457de7af21c6774f380
DanielaOZ <65467658+DanielaOZ@users.noreply.github.com>
2022-10-26 16:43:32 -0500

    Update index.php

--- php-ip-location/index.php
+++ php-ip-location/index.php
@@ -3,4 +3,4 @@
 $var= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
 //var_dump ($var);
 
-echo"La IP del cliente es: ".$var['geoplugin_region'];
+echo"La IP del cliente es: ".$var['geoplugin_countryCode'];
