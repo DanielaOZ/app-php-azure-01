e24e21c3fcdcaafd6be85d0d091c446c00a61c0e
DanielaOZ <65467658+DanielaOZ@users.noreply.github.com>
2022-11-10 20:51:28 -0600

    Update select.php

--- php-ip-location/select.php
+++ php-ip-location/select.php
@@ -1,9 +1,7 @@
 <?php
   include ('connect.php');
   $code=$_GET['code'];
-  $sql = "SELECT country.Name AS Pais, Continent AS Continente, Region, Code, Code2, city.Name AS Ciudad FROM world.country 
-  INNER JOIN world.city ON country.capital=city.ID WHERE country.Code2='$code'";
-
+  $sql = "Exec getPais @code='code'";
 foreach($conn->query($sql) as $row){
     echo "<br> Código: ".$row['Code']."<br>";
     echo "<br> País: ".$row['Pais']."<br>";
